7024f2b9fd55a36e088f716d6131c6d8bda35551
Reshma Hussain <reshnaz91@gmail.com>
2017-12-19 21:45:16 +0530

    Commit

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # S5A2
 
+## Problem Statement
+
+1. Add bootstrap and jquery library in your project.
+2. Refer the node_module for the bootstrap and jquery.
+3. In RootComponent class in @Component ( ) add a metadata for template: string
+and add a button and 1 text box and refer bootstrap class in them.
+
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.5.4.
 
 ## Development server
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,23 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  <div id="login" class="container">
+    <div class="row">
+    <br><br><br>
+      <div class="col-md-4">
+      </div>
+      <div class="form-group col-md-4" style="text-align:center">
+        <h1>Session-5, Assignment-2</h1>
+        <br>
+        <input type="text" class="form-control" placeholder="First Name">
+        <button class="btn btn-primary form-control">Login</button> 
+      </div>
+      <div class="col-md-4">
+      </div>
+    </div>
+  </div>`,
+  // styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'app';
